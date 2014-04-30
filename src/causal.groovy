395c2b439dd6927817b2785238985f463d6c170b
epaull <evanpaull@gmail.com>
2014-04-30 14:11:13 -0700

    --udpates to model with help from Dhanya

--- src/causal.groovy
+++ src/causal.groovy
@@ -17,7 +17,7 @@
 package edu.umd.cs.example;
 
 import edu.umd.cs.psl.application.inference.LazyMPEInference;
-//import edu.umd.cs.psl.application.learning.weight.maxlikelihood.LazyMaxLikelihoodMPE;
+import edu.umd.cs.psl.application.learning.weight.maxlikelihood.LazyMaxLikelihoodMPE;
 import edu.umd.cs.psl.config.*
 import edu.umd.cs.psl.database.DataStore
 import edu.umd.cs.psl.database.Database;
@@ -37,8 +37,6 @@ import edu.umd.cs.psl.model.function.ExternalFunction;
 import edu.umd.cs.psl.ui.functions.textsimilarity.*
 import edu.umd.cs.psl.ui.loading.InserterUtils;
 import edu.umd.cs.psl.util.database.Queries;
-
-import edu.umd.cs.psl.application.learning.weight.em.HardEM;
 
 /* 
  * The first thing we need to do is initialize a ConfigBundle and a DataStore
@@ -69,57 +67,43 @@ println "\t\tDECLARING PREDICATES";
 // atom predicate 
 m.add predicate: "gene"        , types: [ArgumentType.UniqueID, ArgumentType.String]
 
-// target predicate (OPEN): are these genes synthetic lethal? 
-m.add predicate: "sl"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+// target predicate (OPEN): does gene A influence B over a path?
+m.add predicate: "influences"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
 
 // Additional CLOSED target predicates for grounded data
 
-// gene ontologies: distance scores normalized 0 to 1
+// data driven, expression-based from a differential analysis mutation vs not groups
+m.add predicate: "diffExpr"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+
+// data driven, expression-based, correlation in expression 
+m.add predicate: "exprCorr"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+
+// gene ontology based
 m.add predicate: "goCC"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
 m.add predicate: "goMF"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
 m.add predicate: "goBP"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
 
 // physical PPI connections: a heat-diffusion kernel distance, normalized from 0 to 1
-m.add predicate: "ppiConnected"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+m.add predicate: "ppi"	, types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
 
-/* 
- * The 'Enemy of my Enemy is my Friend' Rule
- * This is the key network related rule: if two genes are connected by 2 hops in the sl network, they cannot be
- * sl (i.e directly connected). This could also be called the 'no triangles' rule...
- *
-*/
+// test rules that predict which go category similarities are most predictive. 
+m.add rule : goBP(A,B) & diffExpr(A,B) >> influences(A,B), weight : 1
+m.add rule : goCC(A,B) & diffExpr(A,B) >> influences(A,B), weight : 1
+m.add rule : goMF(A,B) & diffExpr(A,B) >> influences(A,B), weight : 1
 
-// this doesn't seem to work: we need a set comparison
-m.add rule : ( sl(A,X) & sl(B,X) ) >> ~sl(A,B),  weight : 1
-// Require a smaller intersection of the neighborhoods
-//m.add setcomparison: "mutuallyExclusive" , using: SetComparison.Equality, on : sl
-//m.add rule :  mutuallyExclusive( {A.sl + A.sl(inv) } , {B.sl + B.sl(inv) } ) >> sl(A,B) , weight : 3
-
-/*
- * Now we relate this to ontologies: these should be initialized by a correlation analysis of the data
- */
-//m.add rule : ( sl(A,C) & sl(C,B) ) >> goCC(A,B),  weight : 1
-//m.add rule : ( sl(A,C) & sl(C,B) ) >> goMF(A,B),  weight : 1
-//m.add rule : ( sl(A,C) & sl(C,B) ) >> goBP(A,B),  weight : 1
-m.add rule : goBP(A,B) >> sl(A,B), weight : 1
-m.add rule : goCC(A,B) >> sl(A,B), weight : 1
-m.add rule : goMF(A,B) >> sl(A,B), weight : 1
+// test rules that predict which go category similarities are most predictive. 
+m.add rule : goBP(A,B) & diffExpr(A,B) >> influences(A,B), weight : 1
+m.add rule : goCC(A,B) & diffExpr(A,B) >> influences(A,B), weight : 1
+m.add rule : goMF(A,B) & diffExpr(A,B) >> influences(A,B), weight : 1
 
 // 'friends' also likely to be connected in network
-m.add rule : ppiConnected(A,B) >> ~sl(A,B), weight : 1
-
-/*
- * Let's try to add an additional set comparison rule that says similarity in the negative 
- * m.add setcomparison: "connectedsl" , using: SetComparison.Equality, on : sl
- *m.add rule :  ( ~sl(A,B) >> connectedsl( {A.knows + A.knows(inv) } , {B.knows + B.knows(inv) } ) , weight : 3.2
-*/
-
-m.add PredicateConstraint.Symmetric, on : sl
+// encode a function to make this [0,1] where directly connected things are 1
+m.add rule : influences(A,B) >> distance(A,B),  weight : 3
 
 /*
  * Finally, we define a prior on the inference predicate sl. 
  */
-m.add rule: ~sl(A,B), weight: 1
+m.add rule: ~influences(A,B), weight: 1
 
 /*
  * Let's see what our model looks like.
@@ -130,21 +114,17 @@ println m;
 /*
  * Set-up for weight learning and data input 
  */
-// everything we have that's been observed
-Partition readPart = new Partition(0);
-Partition writePart = new Partition(1);
-
-//Partition trainPart = new Partition(0);
+Partition trainPart = new Partition(0);
 Partition truthPart = new Partition(1);
 
 def dir = 'data'+java.io.File.separator+'test'+java.io.File.separator;
 def trainDir = dir+'train'+java.io.File.separator;
 
 // Load static data
-for (Predicate p : [gene, ppiConnected])
+for (Predicate p : [gene])
 {
         println "\t\t\tREADING Ground Variable " + trainDir+p.getName()+".txt";
-	insert = data.getInserter(p, readPart)
+	insert = data.getInserter(p, trainPart)
 	InserterUtils.loadDelimitedData(insert, trainDir+p.getName()+".txt");
 }
 
@@ -153,45 +133,25 @@ for (Predicate p : [gene, ppiConnected])
 for (Predicate p : [goCC, goMF, goBP])
 {
         println "\t\t\tREADING Training Data " + trainDir+p.getName()+".txt";
-	insert = data.getInserter(p, readPart)
+	insert = data.getInserter(p, trainPart)
 	InserterUtils.loadDelimitedDataTruth(insert, trainDir+p.getName()+".txt");
 }
-
-// observed partition
-// write partition
-// 	truth partition are the labels
+	
 
 println "\t\t\tLoading existing sl interactions.."
-insert = data.getInserter(sl, readPart)
+insert = data.getInserter(sl, truthPart)
 InserterUtils.loadDelimitedDataTruth(insert, trainDir+sl.getName()+".txt");
 
 //////////////////////////// weight learning ///////////////////////////
 println "\t\tLEARNING WEIGHTS...";
 
-// set up training DB -- all observed values, SL is there even though you haven't closed it
-// it knows what it needs to learn, and it knows to look for pairs, but if it finds one that 
-// isn't in the input file (A,X) -- populate the database method bypasses that
+Database trainDB = data.getDatabase(trainPart, [gene, goCC, goBP, goMF] as Set);
+Database truthDB = data.getDatabase(truthPart, [sl] as Set);
 
-// PREDICATE when you have all pairs, and when you don't.
-// allInteractions
+LazyMaxLikelihoodMPE weightLearning = new LazyMaxLikelihoodMPE(m, trainDB, truthDB, config);
+weightLearning.learn();
+weightLearning.close();
 
-// im not using labels, link prediction tasks, which is why we're running EM semi-supervised
-// not a supervised task
-
-Database trainDB = data.getDatabase(writePart, [gene, ppiConnected, goCC, goBP, goMF] as Set, readPart);
-Database observedDB = data.getDatabase(readPart, [sl] as Set);
-
-// only works because goXX has all pairs
-DatabasePopulator dbPop = new DatabasePopulator(trainDB);
-// this is looking at goCC, every possible ground atom it has, and take all these pairings. 
-dbPop.populateFromDB(trainDB, goCC);
-
-// now run EM on trainDB: model, random variable DB, training DB and config bundle
-HardEM weightLearning = new HardEM(m, trainDB, observedDB, config);
-
-
-// this might be the right way, but one equivalent would be to create a separate RV with only 
-// inferred SL links
 println "\t\tLEARNING WEIGHTS DONE";
 
 println m
@@ -202,7 +162,7 @@ println "\t\tINFERRING...";
 def testDir = dir+'test'+java.io.File.separator;
 Partition testPart = new Partition(2);
 // Load static data
-for (Predicate p : [gene, sl, ppiConnected])
+for (Predicate p : [gene, sl])
 {
         println "\t\t\tREADING Ground Variable " + testDir+p.getName()+".txt";
 	insert = data.getInserter(p, testPart)
@@ -220,7 +180,7 @@ for (Predicate p : [goCC, goMF, goBP])
 
 
 // don't close the sl interactions this time, but clamp everything else
-Database testDB = data.getDatabase(testPart, [gene, ppiConnected, goCC, goMF, goBP] as Set);
+Database testDB = data.getDatabase(testPart, [gene, goCC, goMF, goBP] as Set);
 LazyMPEInference inference = new LazyMPEInference(m, testDB, config);
 inference.mpeInference();
 inference.close();
