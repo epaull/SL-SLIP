3ce3dc276416cbd91300d6c13a62dbb5ec08a7d8
Evan Paull <evanpaull@gmail.com>
2014-05-29 13:42:48 -0700

    --

--- results/subgraphs/get.sh
+++ results/subgraphs/get.sh
@@ -4,5 +4,5 @@ for file in `find ../../data/yeast/SUBGRAPHS/ -name inferences.txt`; do
 
 	num=`echo $file | sed -e 's/.*SUBGRAPHS\///'	-e 's/\/inferences.txt//'`
 	echo $num
-	mv $file inferences/$num.inferences.txt
+	cp $file inferences/$num.inferences.txt
 done
