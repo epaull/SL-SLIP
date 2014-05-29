681e0ff2002f28d821acef0a19b14b1d51bb3b78
epaull <evanpaull@gmail.com>
2014-05-29 15:10:49 -0700

    --

--- results/subgraphs/plot.R
+++ results/subgraphs/plot.R
@@ -1,8 +1,13 @@
 #!/usr/bin/env Rscript
 
-aucs <- as.matrix(read.table("aucs.txt"))[,2:4]
+aucs <- as.matrix(read.table("aucs.txt",sep="\t",header=TRUE))[,2:9]
 pdf("aucs.pdf")
 plot(aucs[,3],aucs[,2],type='p',col='blue',lwd=2,main="AUC for collective inference (Red) vs non-collective (Blue)")
 lines(aucs[,3],aucs[,1],type='p',col='red',lwd=2)
 abline(h=0.5, lty=2)
+
+pdf("params.pdf")
+plot(aucs[,9], aucs[,2], ylab='AUC', xlab=colnames(aucs)[9], col='red', type='p',xlim=c(0,10))
+
+
 q();
--- results/subgraphs/summarizeResults.py
+++ results/subgraphs/summarizeResults.py
@@ -11,6 +11,7 @@ parser.add_option("--subnet",dest="subnet",action="store_true",default=False)
 baseline_aucs = {}
 aucs = {}
 train_set_size = {}
+inferences = {}
 
 def getAUC(file):
 
@@ -39,10 +40,49 @@ def parseINF(file):
 	#{5.238885973414747} ( CONSIDER(A, B) & PPIEDGES(A, B) ) >> ~( SL(A, B) ) {squared}
 
 	# parse rules: starts with '{', but doesn't include 'consider' in that entry. 
+	state_p = False
+	p_line = None
+
+	weights = {}
 	for line in fh:
 		line = line.rstrip()
 
-	fh.close()			
+		val = None
+		if state_p:
+			if p_line == 10:
+				# friend of friend rule
+				val = line.split(" ")[0].lstrip("{").rstrip("}")
+				weights[p_line] = val
+			elif p_line == 11:
+				# 2 hop PPI/SL
+				val = line.split(" ")[0].lstrip("{").rstrip("}")
+				weights[p_line] = val
+
+			elif p_line == 12:
+				# 2 hop PPI Kernel/SL
+				val = line.split(" ")[0].lstrip("{").rstrip("}")
+				weights[p_line] = val
+
+			elif p_line == 18:
+				# PPI Kernel predictive
+				val = line.split(" ")[0].lstrip("{").rstrip("}")
+				weights[p_line] = val
+
+			elif p_line == 19:
+				# PPI Edges predictive
+				val = line.split(" ")[0].lstrip("{").rstrip("}")
+				weights[p_line] = val
+			elif p_line == 20:
+				fh.close()
+				return weights
+
+			p_line += 1
+
+
+		elif line == "\t\tLEARNING WEIGHTS DONE":
+			state_p = True
+			p_line = 1
+
 
 def countNET(file):
 
@@ -60,14 +100,20 @@ for i in range(1, 51):
 	auc_f = "aucs/"+str(i)+".auc.txt"	
 	base_f = "aucs/"+str(i)+".baseline-auc.txt"	
 	graph = "graphs/"+str(i)+".subgraph_folds.txt"	
+	inf = "inferences/"+str(i)+".inferences.txt"	
 
 	train_set_size[i] = countNET(graph)
 	baseline_aucs[i] = getAUC(base_f)	
 	aucs[i] = getAUC(auc_f)	
+	inferences[i] = parseINF(inf)
 
-
+print "\t".join(["Index", "AUC", "Baseline AUC", "Number Edges", "Friend of Friend", "2-Hop PPI/SL", "2-Hop PPI Kernel/SL", "PPI Kernel Predictive", "PPI Edges Predictive"])
 for i in range(1,51):
 	try:
-		print "\t".join([str(i), aucs[i], baseline_aucs[i], str(train_set_size[i])])
+		printstr = "\t".join([str(i), aucs[i], baseline_aucs[i], str(train_set_size[i])])
+		for j in [10,11,12,18,19]:
+			printstr += "\t"+inferences[i][j]
+		print printstr
 	except:
 		continue
+
